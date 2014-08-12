69246f8bd320701bf81549684ac28c837b8395c5
Dan <dan.morel@gmail.com>
2014-08-12 17:10:48 -0400

    remove body parser

--- hello-byte-world/byte.js
+++ hello-byte-world/byte.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
 
 app.all('/hellobyte', function(req, res) {
   res.send({
